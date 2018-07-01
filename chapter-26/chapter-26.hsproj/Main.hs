02733c36d7a9def39b3c1fc8b9d4419ba01338b6
Timo Denk <github@timodenk.com>
2018-07-01 15:42:54 +0200

    Chapter 26 version update

--- chapter-26/chapter-26.hsproj/Main.hs
+++ chapter-26/chapter-26.hsproj/Main.hs
@@ -5,9 +5,14 @@ import Control.Monad.Except
 import MaybeT
 import ReaderT
 
+import qualified Light2
+
 -- Exercise: Wrap It Up
 -- PDF page 1058
 embedded :: MaybeT 
               (ExceptT String (ReaderT () IO))
               Int
 embedded = MaybeT $ ExceptT $ ReaderT $ (const $ pure (Right (Just 1)))
+
+main :: IO ()
+main = Light2.main
